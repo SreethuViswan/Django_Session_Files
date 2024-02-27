d583b35f17e3cd708bf83d4b696315cdaa2e1418
SreethuViswan <sreethuv0007@gmail.com>
2024-02-27 10:47:28 +0530

    3rd Commit

--- newproject/movieapp/urls.py
+++ newproject/movieapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('details/<int:id>',views.detail,name="details"),
     path('',views.index,name='index'),
     path('add/',views.add,name='add'),
-    path('delete/<int:id>',views.delete,name='delete')
+    # path('delete/<int:id>',views.delete,name='delete')
+    path('update/<int:id>',views.update,name='update')
 
 ]
--- newproject/movieapp/views.py
+++ newproject/movieapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from movieapp.models import Movie
 def detail(request,id):
@@ -43,13 +43,28 @@ def add(request):
         return HttpResponseRedirect('/hello')
     return render(request,'add.html')
 
-def delete(request,id):
-    try:
-        data = Movie.objects.get(pk=id)
-        print(data)
-    except:
-        raise Http404("Does not exist")
-    data.delete()
-    return HttpResponseRedirect('/hello')
-    
+# def delete(request,id):
+#     try:
+#         data = Movie.objects.get(pk=id)
+#         print(data)
+#     except:
+#         raise Http404("Does not exist")
+#     data.delete()
+#     return HttpResponseRedirect('/hello')
+
+def update(request,id):
+    movie = get_object_or_404(Movie,pk=id)
+    if request.method=='POST':
+        tittle = request.POST.get('tittle')
+        year = request.POST.get('year')
+        description = request.POST.get('description')
+        if tittle and year and description:
+            movie.tittle=tittle
+            movie.year=year
+            movie.description=description
+            movie.save()
+            return HttpResponseRedirect('/hello')
+
+    return render(request,'update.html',{'movie':movie})
+
    
